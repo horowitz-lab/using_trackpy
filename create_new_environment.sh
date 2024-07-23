7b5638ab009fb832d5f4ec6a04f604b95b77ed8e
vivarose <vivarose@gmail.com>
2024-07-23 14:13:59 -0400

    Update create_new_environment.sh
    I'm having trouble with the newer version of av.

--- create_new_environment.sh
+++ create_new_environment.sh
@@ -1,8 +1,8 @@
-conda create --name my_env jupyter spyder conda pims numpy cython matplotlib pandas anaconda lmfit
+conda create --name my_env python==3.10 av==9.2.0 jupyter spyder conda pims numpy cython matplotlib pandas anaconda lmfit
 conda activate my_env
 conda update conda --yes
-conda install -c conda forge trackpy ffmpeg tiffile av --yes
-pip install pims moviepy numba seaborn scipy
+conda install -c conda-forge trackpy ffmpeg --yes
+pip install moviepy numba seaborn scipy tiffile 
 pip3 install opencv-python 
 cd Users
 jupyter notebook
