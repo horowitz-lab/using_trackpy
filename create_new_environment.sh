099687eabbcb755eaee77728e7df001f91bdb656
vivarose <vivarose@gmail.com>
2024-07-25 19:43:25 -0400

    FIX av is in conda-forge

--- create_new_environment.sh
+++ create_new_environment.sh
@@ -1,8 +1,9 @@
-conda create --name my_env python==3.10 av==9.2.0 jupyter spyder conda pims numpy cython matplotlib pandas anaconda lmfit
-conda activate my_env
 conda update conda --yes
-conda install -c conda-forge trackpy ffmpeg --yes
-pip install moviepy numba seaborn scipy tiffile 
+conda create --name trackpyenv python==3.10  jupyter spyder conda pims numpy cython matplotlib pandas anaconda --yes
+conda activate trackpyenv 
+conda update conda --yes
+conda install -c conda-forge trackpy ffmpeg av==9.2.0 tifffile  --yes
+pip install moviepy numba seaborn scipy tiffile lmfit
 pip3 install opencv-python 
 cd Users
 jupyter notebook
